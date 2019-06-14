9e1fb6c13194e2eedd370b5623fe2b0ef34f4ff4
quxxk <2830388813@qq.com>
2019-06-14 10:53:51 +0800

    搜索框组件部分

---
+++ components/public/header/user.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="m-user">
+        <template v-if="user">
+            欢迎您，<span class="username">{{ user }}</span>
+            [<nuxt-link to="/exit">退出</nuxt-link>]
+        </template>
+        <template v-else>
+            <nuxt-link class="register" to="/register">立即登录</nuxt-link>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user:'tyqu'
+        }
+    }
+}
+</script>
+
+<style lang='scss'>
+
+</style>
